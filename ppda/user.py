aa4ffc39b47fabe069be472108c4360e82131f0b
Minh Do <dhminh1024@gmail.com>
2024-07-26 14:26:04 +0700

    update: create course class with students and timetable periods

--- ppda/user.py
+++ ppda/user.py
@@ -6,5 +6,5 @@ class User(Base):
     Class to represent a user in SIS system
     """
 
-    ATTRIBUTES = ["name", "full_name", "email"]
+    ATTRIBUTES = ["name", "first_name", "last_name", "email", "password"]
     doctype = "User"
